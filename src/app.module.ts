e21bff9fea754deb3ed2daf7947139ed73fd4201
rsirapidinnovation <rsi@rapidinnovation.dev>
2020-08-26 20:16:18 +0530

    Basic Templates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://fuzz:Pass123#@ds115931.mlab.com:15931/rocksquare')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {MongooseModule} from '@nestjs/mongoose';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.Node_ENV);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
