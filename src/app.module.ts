f16d15808d36b7fdcb2a6c4f99d5a4aed220505d
rsirapidinnovation <rsi@rapidinnovation.dev>
2020-08-27 11:23:56 +0530

    Updating Config File

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {MongooseModule} from '@nestjs/mongoose'
+import {MongooseModule} from '@nestjs/mongoose';
+import 'dotenv/config';
+import {ConfigModule,ConfigService} from '@nestjs/config'
 
+// console.log(process.env.URL)
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://fuzz:Pass123#@ds115931.mlab.com:15931/rocksquare')
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:'.env'
+    }),
+    // MongooseModule.forRoot('mongodb://fuzz:Pass123#@ds115931.mlab.com:15931/rocksquare')
+    // MongooseModule.forRoot(process.env.URL)
+    MongooseModule.forRootAsync({useFactory: (configService: ConfigService) => ({
+          uri:configService.get<string>('URL')
+        }),
+        inject: [ConfigService],})
   ],
   controllers: [AppController],
   providers: [AppService],
