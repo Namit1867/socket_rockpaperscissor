e5c5eba95ba6fa890c8bc26ce4a22dae617190a0
sahil pant <sahilpant16@gmail.com>
2020-08-27 11:46:59 +0530

    added basic setup for websockets

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from '@nestjs/mongoose'
+import { AppgatewayGateway } from './appgateway.gateway';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://fuzz:Pass123#@ds115931.mlab.com:15931/rocksquare')
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppgatewayGateway],
 })
 export class AppModule {}
---
+++ src/appgateway.gateway.ts
@@ -0,0 +1,30 @@
+import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway()
+
+
+export class AppgatewayGateway implements OnGatewayConnection , OnGatewayInit , OnGatewayDisconnect{
+  
+  private logger:Logger = new Logger('AppgatewayGateway')
+
+  @WebSocketServer() wss: Server;
+  
+  handleConnection(client: Socket):void {
+    this.logger.log(`${client.id} connected`)
+  }
+  
+  afterInit(server: Server):void {
+    this.logger.log(`${server}initialised`)
+  }
+  
+  handleDisconnect(client: Socket):void {
+    this.logger.log(`${client.id} disconnected`);
+  }
+  
+  @SubscribeMessage('chat')
+  handleMessage(client: Socket, data: string):void {
+    this.wss.emit('chat',data);
+  }
+}
